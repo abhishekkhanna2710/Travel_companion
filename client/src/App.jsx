3554b59ad406cb8930e733675a8991e5b20f1762
abhishekkhanna2710 <abhikhanna2710@gmail.com>
2023-03-29 13:29:33 +0530

    Creating components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import React from "react"
+import { CssBaseline, Grid } from '@mui/material'
+import Header from "./Components/Header/Header"
+import List from "./Components/List/List"
+import Map from "./Components/Map/Map"
 
 function App() {
 
   return (
     <div className="App">
-      <h1>React</h1>
+      <CssBaseline />
+      <Header />
+
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+
+      </Grid>
+
     </div>
   )
 }
